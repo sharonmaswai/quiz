e0061e186b3373e248961f85298631ab01c9e602
sharonmaswai <chepmaswai@gmail.com>
2019-02-24 23:48:24 +0300

    add custom style for #score

--- js/scripts.js
+++ js/scripts.js
@@ -9,4 +9,19 @@ $("button#submit").click(function() {
   var answer3=parseInt($("input:radio[name=ans3]:checked").val());
   var answer4=parseInt($("input:radio[name=ans4]:checked").val());
   var answer5=parseInt($("input:radio[name=ans5]:checked").val());
-  var sum=(answer1+answer3+answer2+answer4+answer5); 
+  var sum=(answer1+answer3+answer2+answer4+answer5);
+
+if (sum>= 80){
+  document.getElementById("result").innerHTML="You have"+sum +"%";
+  document.getElementById("comment").innerHTML="Congratulations!! You have mastered Javascript."
+
+} else if (sum>= 50) {
+  document.getElementById("result").innerHTML="You have"+sum +"%";
+  document.getElementById("comment").innerHTML="That was a fair attempt.";
+
+} else {
+  document.getElementById("result").innerHTML="You have"+sum +"%";
+  document.getElementById("comment").innerHTML="You failed the test, please retake the test.";
+}
+});
+});
